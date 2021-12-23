dabf3b9c7b3b8549eca56be2f11e06f48eba85d8
soon530 <soon530@gmail.com>
2021-12-23 11:53:51 +0800

    Passing parameters to routes
    1 在navigate後面帶一個object，任何的參數都可以，只要你喜歡就好。
    2 被指定的screen要用route.params去接，不然你傳了沒什麼用呀！
    3 你有接，但沒有傳過來會出錯唷。

--- App.js
+++ App.js
@@ -8,20 +8,33 @@ function HomeScreen({navigation}) {
             <Text>Home Screen</Text>
             <Button
                 title="Go to Details"
-                onPress={() => navigation.navigate('Details')}
+                onPress={() => {
+                    /* 1. Navigate to the Details route with params */
+                    navigation.navigate('Details', {
+                        itemId: 86,
+                        otherParam: 'anything you want here',
+                    });
+                }}
             />
         </View>
     );
 }
 
-function DetailsScreen({navigation}) {
+function DetailsScreen({route, navigation}) {
+    /* 2. Get the param */
+    const {itemId, otherParam} = route.params;
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Details Screen</Text>
+            <Text>itemId: {JSON.stringify(itemId)}</Text>
+            <Text>otherParam: {otherParam}</Text>
             <Button
                 title="Go to Details... again"
-                onPress={() => navigation.push('Details')}
-            />
+                onPress={() =>
+                    navigation.push('Details', {
+                        itemId: Math.floor(Math.random() * 100),
+                    })
+                }/>
             <Button title="Go to Home" onPress={() => navigation.navigate('Home')}/>
             <Button title="Go back" onPress={() => navigation.goBack()}/>
             <Button
