ee1f7ef2203ea09019e40dc2d3c82b94ba5ba466
soon530 <soon530@gmail.com>
2021-12-23 11:33:52 +0800

    Going back goBack()
    加一個按鈕可以回到上一頁，不過左上角本來就有一個上一頁了。

--- App.js
+++ App.js
@@ -22,6 +22,8 @@ function DetailsScreen({navigation}) {
                 title="Go to Details... again"
                 onPress={() => navigation.push('Details')}
             />
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')}/>
+            <Button title="Go back" onPress={() => navigation.goBack()}/>
         </View>
     );
 }
