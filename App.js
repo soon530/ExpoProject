7750c6dd45d9ec9fde8ba9adbf865c8067060c19
soon530 <soon530@gmail.com>
2021-12-23 10:54:22 +0800

    有加入新的套件stack，navigator可以做route，這裡先放1個HomeScreen。

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <StatusBar style="auto"/>
-            </View>
+            <Stack.Navigator>
+                <Stack.Screen name="首頁" component={HomeScreen}/>
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
